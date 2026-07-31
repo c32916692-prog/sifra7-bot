8ac256b71387c942727689a81fea13ac1ec502b2
c32916692-prog <c32916692@gmail.com>
2026-07-31 16:20:32 +0100

    Update nyrox_bot.py

--- nyrox_bot.py
+++ nyrox_bot.py
@@ -24,7 +24,7 @@ from telegram.ext import (
 #  CONFIG
 # ============================================================
 
-BOT_TOKEN        = "8706326366:AAH3ltbkml39u6jY-1u9uJMtYirqwXu5zko"
+BOT_TOKEN        = "8706326366:AAHSb105NwdG6Ugy7df3QPLf34UW3YLe3aQ"
 ADMIN_ID         = 6572004742
 ADMIN_IDS        = (ADMIN_ID,)
 GROUP_CHAT_ID    = -1004205950683
